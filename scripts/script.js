b175cf0db5fca88b7c9ff73a529e7c1ba3dbc6b9
Yasu829 <62891673+Yasu829@users.noreply.github.com>
2021-08-28 04:29:19 +0900

    finished!

--- scripts/script.js
+++ scripts/script.js
@@ -15,6 +15,19 @@ function onYouTubeIframeAPIReady() {
 function YTonload(){
   player_flag = true;
 };
+function setPlaying(){
+  $("#" + $("#MainPlayer").attr("src").substr(30,11)).addClass("playing");
+}
+function MainPlayerStarts(id){
+  $("#MainPlayer").attr("src", "https://www.youtube.com/embed/" + id + "?controls=0&disablekb=1&modestbranding=1&rel=0&"  + String($("#MainPlayer").attr("src")).substr(31));
+  $("#title").html("Playing: " + $("#" + id).text());
+  $("#playing_icon").css("background-image", "url(" + "https://img.youtube.com/vi/" + id + "/default.jpg" + ")")
+}
+function MainPlayerChange(id){
+  $("#MainPlayer").attr("src", "https://www.youtube.com/embed/" + id + String($("#MainPlayer").attr("src")).substr(41));
+  $("#title").html("Playing: " + $("#" + id).text());
+  $("#playing_icon").css("background-image", "url(" + "https://img.youtube.com/vi/" + id + "/default.jpg" + ")")
+}
 function YTGetBackgroundImage(id, times){
   let YTgetimage = new Image();
   let best_url;
@@ -22,24 +35,23 @@ function YTGetBackgroundImage(id, times){
     case 1:
       best_url = "https://img.youtube.com/vi/" + id + "/default.jpg";
       return best_url;
+    case 2:
+      best_url = "https://img.youtube.com/vi/" + id + "/mqdefault.jpg";
       break;
-      case 2:
-        best_url = "https://img.youtube.com/vi/" + id + "/mqdefault.jpg";
-        break;
-        case 3:
-          best_url = "https://img.youtube.com/vi/" + id + "/hqdefault.jpg";
-          break;
-          case 4:
-            best_url = "https://img.youtube.com/vi/" + id + "/sddefault.jpg";
-            break;
-            case 5:
-              best_url = "https://img.youtube.com/vi/" + id + "/maxresdefault.jpg";
-              break;
-            }
-            YTgetimage.onload = function(){
-              let wid = YTgetimage.naturalWidth;
-              if (wid > 120) {
-                $("#Background").css("background-image", "url(" + best_url + ")")
+    case 3:
+      best_url = "https://img.youtube.com/vi/" + id + "/hqdefault.jpg";
+      break;
+    case 4:
+      best_url = "https://img.youtube.com/vi/" + id + "/sddefault.jpg";
+      break;
+    case 5:
+      best_url = "https://img.youtube.com/vi/" + id + "/maxresdefault.jpg";
+      break;
+  }
+  YTgetimage.onload = function(){
+    let wid = YTgetimage.naturalWidth;
+    if (wid > 120) {
+      $("#Background").css("background-image", "url(" + best_url + ")")
     }
     else{
       times--;
@@ -98,14 +110,17 @@ $.getJSON("data/list.json").done(function (json){
   list_flag = true;
   $('.content').on('click', function() {
     if(!flag){
-      $("#MainPlayer").attr("src", "https://www.youtube.com/embed/" + $(this).attr("id") + "?controls=0&disablekb=1&modestbranding=1&rel=0&"  + String($("#MainPlayer").attr("src")).substr(31));
+      MainPlayerStarts(($(this).attr("id")));
       YTGetBackgroundImage(String($("#MainPlayer").attr("src")).substr(30,11), 5);
+      setPlaying();
       setTimeout(function(){MainPlayer.playVideo();},1000);
       flag = true;
     }
     else{
-      $("#MainPlayer").attr("src", "https://www.youtube.com/embed/" + $(this).attr("id") + String($("#MainPlayer").attr("src")).substr(41));
+      $("#" + $("#MainPlayer").attr("src").substr(30,11)).removeClass("playing");
+      MainPlayerChange($(this).attr("id"));
       YTGetBackgroundImage(String($("#MainPlayer").attr("src")).substr(30,11), 5);
+      setPlaying();
       setTimeout(function(){MainPlayer.playVideo();},1000);
     }
     $("#MuSick_logo").on("click", function(){
@@ -117,20 +132,113 @@ $.getJSON("data/list.json").done(function (json){
 }).fail(function(){
   alert("jsonファイルの読み込みに失敗しました");
 });
+function loadFiles(file){
+  let fr = new FileReader();
+  fr.onload = function(event){
+    let jeison = JSON.parse(event.target.result);
+    $("#list").html("");
+    for(let i=0;i<jeison.musics.length;i++){
+      document.getElementById("list").innerHTML += 
+      "<li class='content' id='" + jeison.musics[i].id + "'>" +
+      "<div class='icon_wrapper'>" +
+      "<img class='icon' src='images/ei-music.png'>" +
+      "</div>" +
+      "<div class='name_wrapper'>"+
+      "<p class='name'>"+ jeison.musics[i].name +"</p>"+
+      "</div>"+
+      "</li>";
+    }
+    for(i=0;i<jeison.musics.length;i++){
+      $("#" + jeison.musics[i].id).css("background-image", "url(" + "https://img.youtube.com/vi/" + jeison.musics[i].id + "/default.jpg" + ")")
+    }
+    list_flag = true;
+  $('.content').on('click', function() {
+    if(!flag){
+      MainPlayerStarts(($(this).attr("id")));
+      YTGetBackgroundImage(String($("#MainPlayer").attr("src")).substr(30,11), 5);
+      setPlaying();
+      setTimeout(function(){MainPlayer.playVideo();},1000);
+      flag = true;
+    }
+    else{
+      $("#" + $("#MainPlayer").attr("src").substr(30,11)).removeClass("playing");
+      MainPlayerChange($(this).attr("id"));
+      YTGetBackgroundImage(String($("#MainPlayer").attr("src")).substr(30,11), 5);
+      setPlaying();
+      setTimeout(function(){MainPlayer.playVideo();},1000);
+    }
+    $("#MuSick_logo").on("click", function(){
+      if(MainPlayer_status == 1) MainPlayer.pauseVideo();
+      else if(MainPlayer_status == 2) MainPlayer.playVideo();
+      return false;
+    });
+  });
+  }
+  fr.readAsText(file);
+}
+$(function(){
+  $("#MuSick_logo").on("dragenter dragover", function(event){
+    event.stopPropagation();
+    event.preventDefault();
+    $("#MuSick_logo").css("background-color", "rgb(200,200,200)");
+  });
+  $('#MuSick_logo').on('dragleave', function (event) {
+    event.stopPropagation();
+    event.preventDefault();
+    $('#MuSick_logo').css('background-color', '#242424');
+  });
+  $("#MuSick_logo").on("drop", function(event){
+    event.preventDefault();
+    $("#input_file")[0].files = event.originalEvent.dataTransfer.files;
+    console.log($("#input_file")[0].files);
+    if($("#input_file")[0].files.length > 1){
+      alert("一つまででお願いします");
+      $("#input_file").val("");
+      $("#MuSick_logo").css("background-color", "#242424");
+      return;
+    }
+    else if($("#input_file")[0].files[0].type != "application/json"){
+      alert("jsonファイルでお願いします")
+      $("#MuSick_logo").css("background-color", "#242424");
+      return;
+    }
+    loadFiles($("#input_file")[0].files[0]);
+    $("#MuSick_logo").css("background-color", "#242424");
+  });
+  $("file").on("change", function(){
+    if(this.files.length > 1){
+      alert("一つまででお願いします");
+      $("#input_file").val("");
+      $("#MuSick_logo").css("background-color", "#242424");
+      return;
+    }
+    else if($("#input_file")[0].files[0].type != "application/json"){
+      alert("jsonファイルでお願いします")
+      $("#MuSick_logo").css("background-color", "#242424");
+      return;
+    }
+    loadFiles($("#input_file")[0].files[0]);
+    $("#MuSick_logo").css("background-color", "#242424");
+  });
+});
 let MainPlayer_status = -1;
 setInterval( function(){
   if(player_flag){
     let state = MainPlayer.getPlayerState();
     if ( state == YT.PlayerState.ENDED && MainPlayer_status != 0){
       if($(".content").eq(-1).attr("id") == $("#MainPlayer").attr("src").substr(30,11)) {
-        $("#MainPlayer").attr("src", "https://www.youtube.com/embed/" + $(".content").eq(0).attr("id") + String($("#MainPlayer").attr("src")).substr(41));
+        $("#" + $("#MainPlayer").attr("src").substr(30,11)).removeClass("playing");
+        MainPlayerChange($(".content").eq(0).attr("id"))
         YTGetBackgroundImage(String($("#MainPlayer").attr("src")).substr(30,11), 5);
+        setPlaying();
         next = false;
         setTimeout(function(){MainPlayer.playVideo();},1000);
       }
       else {
-        $("#MainPlayer").attr("src", "https://www.youtube.com/embed/" + $("#" + String($("#MainPlayer").attr("src")).substr(30,11)).next().attr("id") + String($("#MainPlayer").attr("src")).substr(41));
+        $("#" + $("#MainPlayer").attr("src").substr(30,11)).removeClass("playing");
+        MainPlayerChange($("#" + String($("#MainPlayer").attr("src")).substr(30,11)).next().attr("id"));
         YTGetBackgroundImage(String($("#MainPlayer").attr("src")).substr(30,11), 5);
+        setPlaying();
         next = false;
         setTimeout(function(){MainPlayer.playVideo();},1000);
       }
