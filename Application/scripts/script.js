91ac61136dfabf3bd8335e3c76ee0a8785af8d5f
Yasu829 <62891673+Yasu829@users.noreply.github.com>
2022-09-05 16:19:35 +0900

    Update script.js

--- Application/scripts/script.js
+++ Application/scripts/script.js
@@ -52,7 +52,7 @@ function MainPlayerReset(){
   $(".playing").removeClass("playing");
   MainPlayer.pauseVideo();
   if(!flag){
-    $("#MainPlayer").attr("src", "https://www.youtube.com/embed/" + "-----------" + "?playlist=" + "-----------" +"&fs=0&loop=1&controls=1&disablekb=1&modestbranding=1&rel=0&"  + String($("#MainPlayer").attr("src")).substr(31));
+    $("#MainPlayer").attr("src", "https://www.youtube.com/embed/" + "-----------" + "?playlist=" + "-----------" +"&fs=0&loop=0&controls=1&disablekb=1&modestbranding=1&rel=0&"  + String($("#MainPlayer").attr("src")).substr(31));
     flag = true;
   }
   else{
@@ -62,7 +62,7 @@ function MainPlayerReset(){
 }
 
 function MainPlayerStarts(id){
-  $("#MainPlayer").attr("src", "https://www.youtube.com/embed/" + id + "?playlist=" + id +"&fs=0&loop=1&controls=1&disablekb=1&modestbranding=1&rel=0&"  + String($("#MainPlayer").attr("src")).substr(31));
+  $("#MainPlayer").attr("src", "https://www.youtube.com/embed/" + id + "?playlist=" + id +"&fs=0&loop=0&controls=1&disablekb=1&modestbranding=1&rel=0&"  + String($("#MainPlayer").attr("src")).substr(31));
   $("#title").html("Playing: " + $("#" + id).text());
   // $("#playing_icon").css("background-image", "url(" + "https://img.youtube.com/vi/" + id + "/default.jpg" + ")")
 }
